a6d524f6fb4816b01ef0d374529824d43e6dfaa5
Jonathan McGlone <jmcglone@umich.edu>
2013-12-11 10:06:37 -0500

    Create library.js
    File to store functions. Added create_colorSwatch, function that turns hex color value into css background.

---
+++ js/library.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  
+  init_document_ready();
+
+});
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - 
+/* - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Anything that should be run as soon as DOM is 
+ * loaded.
+ * - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+ 
+function init_document_ready() {
+
+  create_colorSwatch();
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+// for each li.color, grab the hex color value in the item and set is at the background-color 
+
+function create_colorSwatch() {
+
+  $( "li.color" ).each(function() {
+    
+    var color = $( this ).text();
+    $( this ).css( "background-color" , color );
+    
+  });
+
+}
