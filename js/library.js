070e2d714d99d91b6a55deff29d5b94d23352f8b
Jonathan McGlone <jmcglone@umich.edu>
2013-12-12 11:21:26 -0500

    Update library.js
    added if statement to calculate rgb and replace font color if swatch color is too dark.

--- js/library.js
+++ js/library.js
@@ -27,6 +27,24 @@ function create_colorSwatch() {
     var color = $( this ).text();
     $( this ).css( "background-color" , color );
     
+    //if the single swatch color is too dark, use a lighter font color to display the hex color value
+    
+    var c = color.substring(1);      // strip #
+    var rgb = parseInt(c, 16);   // convert rrggbb to decimal
+    var r = (rgb >> 16) & 0xff;  // extract red
+    var g = (rgb >>  8) & 0xff;  // extract green
+    var b = (rgb >>  0) & 0xff;  // extract blue
+
+    var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+
+    if (luma < 85) {
+      $( this ).css( "color" , "#f0f0f0" );
+    }
+    
   });
 
 }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
